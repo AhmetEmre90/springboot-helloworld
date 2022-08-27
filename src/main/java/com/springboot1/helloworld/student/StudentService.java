0a3523c1572907216a195bbdcc5383a12865a213
AhmetEmre90 <amermer05@hotmail.com>
2022-08-27 12:14:12 +0300

    put method implemented for student, get method updated to order students by id

--- src/main/java/com/springboot1/helloworld/student/StudentService.java
+++ src/main/java/com/springboot1/helloworld/student/StudentService.java
@@ -1,9 +1,12 @@
 package com.springboot1.helloworld.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -17,7 +20,7 @@ public class StudentService {
     }
 
     public List<Student> getStudents() {
-        return studentRepository.findAll();
+        return studentRepository.findAll(Sort.by("id"));
     }
 
     public void addNewStudent(Student student) {
@@ -40,4 +43,23 @@ public class StudentService {
         studentRepository.deleteById(id);
     }
 
+    @Transactional
+    public void updateStudent(Long id, String name, String email) {
+        Optional<Student> studentOptional = studentRepository.findById(id);
+
+        if (studentOptional.isEmpty()) {
+            throw new IllegalStateException("student doesn't exist");
+        }
+
+        if (name != null && name.length() > 0 && !Objects.equals(studentOptional.get().getName(), name)) {
+            studentOptional.get().setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(studentOptional.get().getEmail(), email)) {
+            if (studentRepository.findStudentByEmail(email).isPresent()) {
+                throw new IllegalStateException("email taken, can't update the email");
+            }
+            studentOptional.get().setEmail(email);
+        }
+    }
 }
